67e1e9073536cece75dcd07af6f0dcfef1f50200
Phan Nguyễn Anh Kiệt <anhkietphan2402.tg@gmail.com>
2025-07-25 15:41:05 +0700

    bai 95

--- src/Layout/DefaultLayout/Sidebar/index.js
+++ src/Layout/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,11 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+const cx = classNames.bind(styles);
 function Siderbar() {
     return (
-        <h2>Siderbar</h2>
+        <aside className={cx("wrapper")}>
+            <h2>Sidebar</h2>
+        </aside>
     );
 }
 

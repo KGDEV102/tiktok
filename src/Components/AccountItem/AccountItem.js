6a5874636d6fcc43c9b89cb465ec8b44ffb88511
Phan Nguyễn Anh Kiệt <anhkietphan2402.tg@gmail.com>
2025-08-04 14:17:11 +0700

    update

--- src/Components/AccountItem/AccountItem.js
+++ src/Components/AccountItem/AccountItem.js
@@ -1,12 +1,12 @@
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss"
 import { BsCheckCircleFill } from 'react-icons/bs';
-
+import Image from "~/Layout/components/Image";
 const cx = classNames.bind(styles);
 function AccountItem() {
     return ( 
         <div className={cx("wrapper")}>
-            <img className={cx("avartar")}alt="User Avatar" src="https://p9-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/d5450dffd6596cb0302441b62d5c2663~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=8e71a4ef&x-expires=1753786800&x-signature=bSYPVvJ681nEP9ok7dwNb6AiYqs%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"></img>
+            <Image className={cx("avartar")}alt="User Avatar" src="https://p9-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/d5450dffd6596cb0302441b62d5c2663~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=8e71a4ef&x-expires=1753786800&x-signature=bSYPVvJ681nEP9ok7dwNb6AiYqs%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"></Image>
             <div className={cx("infor")}>
                 <h4 className={cx("name")}>KGDEV
                     <BsCheckCircleFill className={cx("check")}></BsCheckCircleFill>
